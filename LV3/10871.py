bd8bba60b981e496780529ea6e076d0ded8c742b
Mm6585 <101806955+Mm6585@users.noreply.github.com>
2022-04-16 16:34:13 +0900

    10871 solution

---
+++ LV3/10871.py
@@ -0,0 +1,10 @@
+n, x = map(int, input().split())
+array = list(map(int, input().split()))
+result = []
+
+for i in range(n):
+  if (array[i] < x):
+    result.append(array[i])
+
+for i in range(len(result)):
+  print(result[i], end=' ')
