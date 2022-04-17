15ab5d00fa75c14b402e25bbc34ab064b07e7b61
Mm6585 <101806955+Mm6585@users.noreply.github.com>
2022-04-18 03:31:44 +0900

    1065 solution

---
+++ LV5/1065.py
@@ -0,0 +1,16 @@
+def f(x):
+    x = str(x)
+    a = []
+    for i in range(len(x)-1):
+        a.append(int(x[i+1]) - int(x[i]))
+    a = set(a)
+    if (len(a) != 1 and len(a) != 0):
+        return 0
+    else:
+        return 1
+
+n = int(input())
+cnt = 0
+for i in range(n):
+    cnt += f(i+1)
+print(cnt)
