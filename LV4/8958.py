d015c377e94f5d682a42057a1c1c5d2c4884c128
Mm6585 <101806955+Mm6585@users.noreply.github.com>
2022-04-17 11:19:10 +0900

    8958 solution

---
+++ LV4/8958.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+for i in range(n):
+    a = list(input())
+    sum = 0
+    score = 0
+    
+    for o in a:
+        if (o == 'O'):
+            score+=1
+            sum+=score
+        else:
+            score = 0
+    print(sum)
