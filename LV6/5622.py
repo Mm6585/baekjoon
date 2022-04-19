39b5913c0ab2767c83bc4289cc27d9fecf701099
Mm6585 <101806955+Mm6585@users.noreply.github.com>
2022-04-19 19:42:56 +0900

    5622 solution

---
+++ LV6/5622.py
@@ -0,0 +1,11 @@
+s = input()
+alp = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
+time = 0
+
+for c in s:
+    for i in range(len(alp)):
+        for j in range(len(alp[i])):
+            if (c == alp[i][j]):
+                time += i + 3
+    
+print(time)
