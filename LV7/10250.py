7298ea4427420e64912f8c023994078ea89a58c6
Mm6585 <101806955+Mm6585@users.noreply.github.com>
2022-04-24 17:38:18 +0900

    10250 solution

---
+++ LV7/10250.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+for i in range(t):
+    h, w, n = map(int, input().split())
+    a = n % h
+    if (a == 0):
+        a = h
+        b = n // h
+    else:
+        b = n // h + 1
+    print(a*100 + b)
