4f6a9044f7b89f972d0c62dc13f8593db12efc97
Mm6585 <101806955+Mm6585@users.noreply.github.com>
2022-05-20 15:41:03 +0900

    7568 solution

---
+++ LV10/7568.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+l = []
+for _ in range(n):
+    l.append(list(map(int, input().split())))
+
+for i in l:
+    rank = 1
+    for j in l:
+        if (i[0] < j[0] and i[1] < j[1]):
+            rank += 1
+    print(rank, end=' ')
