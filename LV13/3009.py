2a901f593475536ce4c9650dbbb0cd857b390659
Mm6585 <101806955+Mm6585@users.noreply.github.com>
2022-06-16 15:09:20 +0900

    3009 solution

---
+++ LV13/3009.py
@@ -0,0 +1,20 @@
+x = dict()
+y = dict()
+for _ in range(3):
+    a, b = map(int, input().split())
+    if (a in x):
+    	x[a] += 1
+    else:
+    	x[a] = 1
+    if (b in y):
+    	y[b] += 1
+    else:
+    	y[b] = 1
+for i in x:
+	if (x[i] == 1):
+		print(i, end=' ')
+		break
+for i in y:
+	if (y[i] == 1):
+		print(i)
+		break
