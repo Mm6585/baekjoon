791bb6ceb743a770ef8141ab37eef43376c2d3a3
Mm6585 <101806955+Mm6585@users.noreply.github.com>
2022-06-17 20:22:11 +0900

    4153 solution

---
+++ LV13/4153.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+while (True):
+    l = list(map(int, input().split()))
+    if (l[0] == 0):
+        break
+    l.sort()
+    
+    if (l[0]**2 + l[1]**2 == l[2]**2):
+        print('right')
+    else:
+        print('wrong')
